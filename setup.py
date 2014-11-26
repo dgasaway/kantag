c319626e72caa70f3c4be4e6ebbff38dd4269776
David Gasaway <dave@gasaway.org>
2014-11-25 22:14:34 -0800

    Fix setup.py and move to setuptools.

--- setup.py
+++ setup.py
@@ -1,19 +1,32 @@
-from distutils.core import setup
+from setuptools import setup
 from kantag._version import __version__
 
 setup(
     name='kantag',
     version=__version__,
-    author='David Gasaway',
-    author_email='dave@gasaway.org',
-    packages=['kantag'],
-    scripts=['bin/applykan', 'bin/initkan', 'bin/showkan'],
-    url='http://code.google.com/c/kantag/',
-    license='GNU GPL v2',
     description='manage audio file metadata using external text files',
     long_description=open('README.txt').read(),
+    author='David Gasaway',
+    author_email='dave@gasaway.org',
+    url='https://code.google.com/p/kantag/',
+    download_url='https://code.google.com/p/kantag/downloads/list',
+    license='GNU GPL v2',
+    packages=['kantag'],
+    scripts=['bin/applykan', 'bin/initkan', 'bin/showkan'],
     install_requires=[
-		"mutagen >= 1.20"
-        "python-musicbrainz2 >= 0.7.2"
+        'mutagen >= 1.20',
+        'python-musicbrainz2 >= 0.7.2',
+    ],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Indended Audience :: End Users/Desktop',
     ],
 )
